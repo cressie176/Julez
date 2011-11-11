4153236f2da5508557fd328743bfafa35a1f294a
Stephen Cresswell <github@stephen-cresswell.net>
2011-11-11 22:59:01 +0000

    Improving readme

--- src/test/java/uk/co/acuminous/julez/CombinationPerformanceTest.java
+++ src/test/java/uk/co/acuminous/julez/CombinationPerformanceTest.java
@@ -10,7 +10,7 @@ public class CombinationPerformanceTest {
     private static final int TEST_DURATION = 15;
 
     @Test
-    public void testTheSystemSupportsDifferentScearnios() throws Throwable {
+    public void testTheSystemSupportsRunningDifferentScenariosInParallel() throws Throwable {
 
         ConcurrentTestRunner runner1 = new ConcurrentTestRunner(new HelloWorldScenario(), MAX_THROUGHPUT, TEST_DURATION);
         ConcurrentTestRunner runner2 = new ConcurrentTestRunner(new GoodbyeWorldScenario(), MAX_THROUGHPUT, TEST_DURATION);
@@ -18,8 +18,11 @@ public class CombinationPerformanceTest {
         MultiConcurrentTestRunner multiTestRunner = new MultiConcurrentTestRunner(runner1, runner2);
         multiTestRunner.run();
 
-        assertTrue(String.format("Actual throughput: %d scenarios per second", runner1.actualThroughput()), runner1.actualThroughput() >= 20);
-        assertTrue(String.format("Actual throughput: %d scenarios per second", runner2.actualThroughput()), runner2.actualThroughput() >= 20);
+        int throughput1 = runner1.actualThroughput();
+        assertTrue(String.format("Actual throughput: %d scenarios per second", throughput1), throughput1 >= 20);
+        
+        int throughput2 = runner2.actualThroughput();
+        assertTrue(String.format("Actual throughput: %d scenarios per second", throughput2), throughput2 >= 20);
     }
 
     class HelloWorldScenario implements Scenario {
