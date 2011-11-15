dcbd202e908d2150fa42ce78effb6f5e8c7f8b05
Frank Carver <frank.carver@googlemail.com>
2011-11-15 10:27:32 +0000

    removed redundant methods from ResultRecorder

--- src/main/java/uk/co/acuminous/julez/recorder/ResultRecorder.java
+++ src/main/java/uk/co/acuminous/julez/recorder/ResultRecorder.java
@@ -3,12 +3,4 @@ package uk.co.acuminous.julez.recorder;
 import uk.co.acuminous.julez.ResultSink;
 
 public interface ResultRecorder extends ResultSink, ResultExaminer {
-    int passCount();
-    int failureCount();
-    int percentage();    
-    void pass();
-    void pass(String description);
-    void fail(String description);
-    void shutdownGracefully();
-    void complete(int timeout);
 }
