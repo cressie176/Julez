9d37e4d76d5277c75fbb7d19270c7390c451ab31
Frank Carver <frank.carver@googlemail.com>
2011-11-25 14:38:56 +0000

    Event notes

--- julez-core/src/main/java/uk/co/acuminous/julez/event/Event.java
+++ julez-core/src/main/java/uk/co/acuminous/julez/event/Event.java
@@ -7,18 +7,34 @@ import java.util.UUID;
 import com.google.gson.Gson;
 
 public abstract class Event {
-    
     public static final String EVENT_TYPE_FORMAT = "%s/%s";    
     
+    // TODO I find it puzzling that you provide a no-arg constructor and yet 
+    // have no setters for these fields. If they are OK being null, why not just put 
+    // them in the data map?
     private String id;
     private long timestamp;    
     private String type;
     private String correlationId;
-    private Map<String, String> data = new HashMap<String, String>();
+    
+    // TODO I'm intrigued by your choice of String as the value type for this Map
+    // are you really expecting anything put in here to be clumsily serialised to 
+    // String and back just to be passed along an in-memory handler chain? 
+    private Map<String, String> data;
 
-    public Event() {        
+    // NOTE I have added a constructor which allows choice of a different Map implementation
+    // Feel free to discuss, but for me this is a no-brainer.
+    public Event(Map<String, String> data) {
+    	setData(data);
     }
     
+    public Event() {
+    	this(new HashMap<String, String>());
+    }
+    
+    // TODO as this is such a low-level class I would not bother with this constructor
+    // By all means have a subclass (UUIDEvent or whatever) which provides it as a convenience
+    // but by placing it here you are implying that all uses of Event will use a UUID for id
     public Event(String type, String correlationId) {
         this(UUID.randomUUID().toString(), System.currentTimeMillis(), type, correlationId);
     }
@@ -54,14 +70,21 @@ public abstract class Event {
         this.data = data;
     }
     
+    // TODO _please_ use a more sensible implementation. 
+    // IMHO?, toString is mainly for developer examination.
+    // Attempting to live with the constraints of an external representation usually causes 
+    // more problems than it solves.
     public String toString() {
         return toJson();
     }
     
+    // TODO converting to and from any specific representation does not belong in here.
+    // And worse, they even drag in dependencies and _force_ a specific implementation.
+    // This Event class should be the most flexible and least dependent bit of the whole system.  
+    // What were you thinking !
     public String toJson() {
         return new Gson().toJson(this);
     }    
-    
     public Event fromJson(String json) {
         return new Gson().fromJson(json, this.getClass());        
     }
