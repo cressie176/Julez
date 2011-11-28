3a0fc6d532ffa875ca4d210e4982968edb807324
Stephen Cresswell <github@stephen-cresswell.net>
2011-11-28 22:46:24 +0000

    Adding EventPipe interface

--- julez-core/src/main/java/uk/co/acuminous/julez/event/handler/EventRelay.java
+++ julez-core/src/main/java/uk/co/acuminous/julez/event/handler/EventRelay.java
@@ -2,9 +2,9 @@ package uk.co.acuminous.julez.event.handler;
 
 import uk.co.acuminous.julez.event.BaseEventSource;
 import uk.co.acuminous.julez.event.Event;
-import uk.co.acuminous.julez.event.EventHandler;
+import uk.co.acuminous.julez.event.EventPipe;
 
-public class EventRelay extends BaseEventSource implements EventHandler {
+public class EventRelay extends BaseEventSource implements EventPipe {
 
     @Override
     public void onEvent(Event event) {
